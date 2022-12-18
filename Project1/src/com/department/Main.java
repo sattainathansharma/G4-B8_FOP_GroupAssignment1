ae0c1687fec6441bc5eff9b3175c140538da9e76
sattainathansharma <119773786+sattainathansharma@users.noreply.github.com>
2022-12-18 10:19:35 +0530

    Add files via upload
    Submitted group assignment 1 on behalf of G4-B8 team.

---
+++ Project1/src/com/department/Main.java
@@ -0,0 +1,31 @@
+package com.department;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		HrDepartment hr = new HrDepartment();
+		TechDepartment tech = new TechDepartment();
+		AdminDepartment ad = new AdminDepartment();
+
+		System.out.println("Welcome to " + ad.departmentName());
+		System.out.println(ad.getTodaysWork());
+		System.out.println(ad.getWorkDeadline());
+		System.out.println(ad.isTodayAHoliday());
+		System.out.println();
+
+		System.out.println("Welcome to " + hr.departmentName());
+		System.out.println(hr.doActivity());
+		System.out.println(hr.getTodaysWork());
+		System.out.println(hr.getWorkDeadline());
+		System.out.println(hr.isTodayAHoliday());
+		System.out.println();
+
+		System.out.println("Welcome to " + tech.departmentName());
+		System.out.println(tech.getTodaysWork());
+		System.out.println(tech.getWorkDeadline());
+		System.out.println(tech.getTechStackInformation());
+		System.out.println(tech.isTodayAHoliday());
+
+	}
+}
